daea9761833e38b1a8397f46df2747a7d525946f
Tim <tim_stassen@live.nl>
2024-05-01 11:04:28 +0200

    update morning 1-5-2024

---
+++ code/preprocessPCCT.py
@@ -0,0 +1,33 @@
+# build code that preprocesses the PCCT data
+
+# remove surroundings
+# merge into one 3D array/tensor
+# prepare to elastix readible format. 
+
+import os
+import SimpleITK as sitk
+import numpy as np
+
+def order_by_slice_location(slice):
+    return float(slice.GetMetaData('0020|1041'))
+
+
+def create3D_input(path):
+    dicom_filenames = os.listdir(path)
+
+    # only include the non-constrast images
+    list_of_slices = []
+    for filename in dicom_filenames:
+        dicom_path = os.path.join(path, filename)
+        slice = sitk.ReadImage(dicom_path)
+        if 'Monoenergetic Plus Knee Noncontrast#0' in slice.GetMetaData('0008|103e'):
+            list_of_slices.append(slice)
+
+    list_of_slices.sort(key=order_by_slice_location, reverse=True)
+
+    volume_list = []
+    for slice in list_of_slices:
+        volume_list.append(sitk.GetArrayViewFromImage(slice)[0])
+    image_array = np.array(volume_list)
+    
+    return sitk.GetImageFromArray(image_array)
---
+++ code/test_code/elastix_test_own_data.py
@@ -0,0 +1,65 @@
+from __future__ import print_function, absolute_import
+import elastix
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+import os
+import elastix
+
+
+root_folder = r'C:\Users\T2025\Desktop\cadaver_knee_study\data\PCCT'
+data_path = r'C:\Users\T2025\Desktop\cadaver_knee_study\data\PCCT\ME_data\07_2017'
+
+
+# IMPORTANT: these paths may differ on your system, depending on where
+# Elastix has been installed. Please set accordingly.
+ELASTIX_PATH = os.path.join(r'C:\Tim\Software\Elastix\elastix.exe')
+TRANSFORMIX_PATH = os.path.join(r'C:\Tim\Software\Elastix\transformix.exe')
+
+if not os.path.exists(ELASTIX_PATH):
+    raise IOError('Elastix cannot be found, please set the correct ELASTIX_PATH.')
+if not os.path.exists(TRANSFORMIX_PATH):
+    raise IOError('Transformix cannot be found, please set the correct TRANSFORMIX_PATH.')
+
+# Make a results directory if non exists
+if os.path.exists('results_own_data') is False:
+    os.mkdir('results_own_data')
+
+moving_image_path = r'C:\Users\T2025\Desktop\cadaver_knee_study\code\test_code\moving_im.jpg'
+fixed_image_path = r'C:\Users\T2025\Desktop\cadaver_knee_study\code\test_code\fixed_im.jpg'
+
+
+el = elastix.ElastixInterface(elastix_path=ELASTIX_PATH)
+el.register(
+    fixed_image=fixed_image_path,
+    moving_image=moving_image_path,
+    parameters=[r'C:\Users\T2025\Desktop\cadaver_knee_study\code\config\elastix\parameters_bspline_multires_MR.txt'],
+    output_dir='results_own_data')
+
+# result_path = os.path.join(root_folder,'results', 'result.0.tiff')
+
+# Find the results
+transform_path = os.path.join('results_own_data', 'TransformParameters.0.txt')
+result_path = os.path.join('results_own_data', 'result.0.tiff')
+
+# Open the logfile into the dictionary log
+for i in range(5):
+    log_path = os.path.join('results_own_data', 'IterationInfo.0.R{}.txt'.format(i))
+    log = elastix.logfile(log_path)
+    # Plot the 'metric' against the iteration number 'itnr'
+    plt.plot(log['itnr'], log['metric'])
+plt.legend(['Resolution {}'.format(i) for i in range(5)])
+
+# Load the fixed, moving, and result images
+fixed_image = imageio.v2.imread(fixed_image_path)[:, :, 0]
+moving_image = imageio.v2.imread(moving_image_path)[:, :, 0]
+transformed_moving_image = imageio.v2.imread(result_path)
+
+# Show the resulting image side by side with the fixed and moving image
+fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+ax[0].imshow(fixed_image, cmap='gray')
+ax[0].set_title('Fixed image')
+ax[1].imshow(moving_image, cmap='gray')
+ax[1].set_title('Moving image')
+ax[2].imshow(transformed_moving_image, cmap='gray')
+ax[2].set_title('Transformed\nmoving image')
