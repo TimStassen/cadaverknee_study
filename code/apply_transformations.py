cb71e05a3f12105fe3558b614274bf1fd3fbc4c1
TStue <99491579+TStue@users.noreply.github.com>
2024-05-31 20:56:58 +0200

    update (issue with transformix)

--- code/apply_transformations.py
+++ code/apply_transformations.py
@@ -12,19 +12,18 @@ atlas_inputs = ['17_2016', '07_2017', '30_2017']
 a = Atlas(inputs = atlas_inputs)
 
 moving_images = ['08_2017', '08_2017R', '10_2019', '12_2018', 
-                 '18_2018', '29_2017', '30_2017']
+                 '18_2018', '29_2017', '01_2019']
 
-elastix_path = os.path.join(r'C:\Tim\Software\Elastix\elastix.exe')
+elastix_path = os.path.join(r'C:\Users\20201900\Desktop\Master BME\8DM20\elastix\elastix.exe')
 transformix_path = os.path.join(r'C:\Users\20201900\Desktop\Master BME\8DM20\elastix\transformix.exe')
 affine_parameter_file = r'C:\Users\T2025\Desktop\cadaver_knee_study\code\elastix_parameter_files\parameters_affine_atlas.txt'
 bspline_parameter_file = r'C:\Users\T2025\Desktop\cadaver_knee_study\code\elastix_parameter_files\parameters_bspline_multires.txt'
-
 
 registrations_dir_path = r'D:\atlas_registration_results_bone_segs'
 registrations_dir = os.listdir(registrations_dir_path)
 segmentation_dir = r'D:\segmentations PCCT Tim\cartilage_PCCT_atlas'
 for registration in registrations_dir:
-
+    # pdb.set_trace()
     parts_registration_name = registration.split('_')
     anatomical_struct = parts_registration_name[-1]
     if anatomical_struct == 'femur':
@@ -36,10 +35,9 @@ for registration in registrations_dir:
     else: 
           continue
         
-        
     seg_img_name = parts_registration_name[2] + '_' + parts_registration_name[3]
-    seg_img_dir = os.path.join(segmentation_dir, seg_img_name, seg_img_name + '_' + cartilage + '.mhd') # or mhd if it is converted
-    pdb.set_trace()
+    seg_img = os.path.join(segmentation_dir, seg_img_name, seg_img_name + '_' + cartilage + '.mhd') # or nrrd if it is not converted
+    # pdb.set_trace()
 
     # transform_matrix0 = os.path.join(registrations_dir_path, registration, 'TransformParameters.0.txt')
     transformation_file_dir = os.path.join(registrations_dir_path, registration)
@@ -49,7 +47,7 @@ for registration in registrations_dir:
     #                         fixed_image=fixed_image, atlas_path=atlas_path, 
     #                         fixed_im_mask=fixed_mask, results_path=results_path)
     
-    a.apply_transformation(transformation_file_dir, seg_img_dir, fixed_image=None, moving_image=None, transformix_path=transformix_path)
+    a.apply_transformation(transformation_file_dir, seg_img, fixed_image=None, moving_image=None, transformix_path=transformix_path)
 
 
 
