99fdda685b2641037de075106f5b36cb930fd246
Tim <tim_stassen@live.nl>
2024-06-17 22:46:16 +0200

    17_6

--- code/utils/overlap_metrics.py
+++ code/utils/overlap_metrics.py
@@ -10,9 +10,9 @@ import pdb
 
 
 class AnalyzeVolume:
-    def __init__(self, GT_list, pred_list, fixed_img_name, moving_img_name):
-        self.GT_list = GT_list
-        self.pred_list = pred_list
+    def __init__(self, GT, pred, fixed_img_name, moving_img_name):
+        self.GT = GT
+        self.pred = pred
         self.fixed_img_name = fixed_img_name
         self.moving_img_name = moving_img_name
 
@@ -22,37 +22,39 @@ class AnalyzeVolume:
         result_list = []
         # fixed_image = []
         # moving_image = []
-        for pred, GT, fixed_name, moving_name in zip(self.pred_list, self.GT_list, self.fixed_img_name, self.moving_img_name):
-            pred_flat = np.ndarray.flatten(pred)
-            true_flat = np.ndarray.flatten(GT)
+        # for pred, GT, fixed_name, moving_name in zip(self.pred, self.GT, self.fixed_img_name, self.moving_img_name):
+        fixed_name = self.fixed_img_name
+        moving_name = self.moving_img_name
 
-            mcm = confusion_matrix(pred_flat, true_flat)
-            tn = mcm[0, 0]
-            fp = mcm[0, 1]
-            fn = mcm[1, 0]
-            tp = mcm[1, 1]
+        pred_flat = np.ndarray.flatten(self.pred)
+        true_flat = np.ndarray.flatten(self.GT)
 
-            # Compute the evaluation metrics.
-            Sensitivity = tp / (tp + fn)
-            Specificity = tn / (tn + fp)
-            Accuracy = (tp + tn) / (tp + tn + fn + fp)
-            Dice_score = 2 * tp / (2 * tp + fp + fn)
-            pdb.set_trace()
-            Precision = tp / (tp + fp)
-            Recall = tp / (tp + fn)
-            F1 = 2 * (Recall * Precision) / (Recall + Precision)
+        mcm = confusion_matrix(pred_flat, true_flat)
+        tn = mcm[0, 0]
+        fp = mcm[0, 1]
+        fn = mcm[1, 0]
+        tp = mcm[1, 1]
+
+        # Compute the evaluation metrics.
+        Sensitivity = tp / (tp + fn)
+        Specificity = tn / (tn + fp)
+        Accuracy = (tp + tn) / (tp + tn + fn + fp)
+        Dice_score = 2 * tp / (2 * tp + fp + fn)
+        pdb.set_trace()
+        Precision = tp / (tp + fp)
+        Recall = tp / (tp + fn)
+        F1 = 2 * (Recall * Precision) / (Recall + Precision)
 
 
-            dict = {'fixed_image': fixed_name, 
-                    'moving_image': moving_name,
-                    'Sensitivity': Sensitivity,
-                    'Specificity': Specificity,
-                    'Accuracy': Accuracy,
-                    'Dice_score': Dice_score,
-                    'F1': [F1]}
+        dict = {'fixed_image': fixed_name, 
+                'moving_image': moving_name,
+                'Sensitivity': Sensitivity,
+                'Specificity': Specificity,
+                'Accuracy': Accuracy,
+                'Dice_score': Dice_score,
+                'F1': [F1]}
 
-            result_list.append(dict)
-        return result_list
+        return dict
 
 
     # def create_df(self, inner_loop_pred_segm, inner_loop_gt_segm, train_set):
@@ -105,25 +107,25 @@ class AnalyzeVolume:
             print('For', metrics[i], ',the average is:', np.mean(data[i]), 'and the std is:', np.std(data[i]))
         return None
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    GT_vol1 = np.array([[1, 0, 0], [0, 1, 1], [0, 1, 1]])
-    GT_vol2 = np.array([[1, 1, 0], [0, 0, 1], [0, 1, 0]])
-    GT_vol3 = np.array([[0, 0, 0], [1, 1, 1], [0, 1, 1]])
-    GT_tot = [GT_vol1, GT_vol2, GT_vol3]
+#     GT_vol1 = np.array([[1, 0, 0], [0, 1, 1], [0, 1, 1]])
+#     GT_vol2 = np.array([[1, 1, 0], [0, 0, 1], [0, 1, 0]])
+#     GT_vol3 = np.array([[0, 0, 0], [1, 1, 1], [0, 1, 1]])
+#     GT_tot = [GT_vol1, GT_vol2, GT_vol3]
 
-    pred_vol1 = np.array([[1, 1, 0], [0, 1, 1], [0, 0, 1]]) # acc = 7/9
-    pred_vol2 = np.array([[1, 1, 0], [0, 0, 1], [1, 1, 0]]) #
-    pred_vol3 = np.array([[1, 0, 0], [1, 0, 1], [0, 1, 0]]) #
-    pred_tot = [pred_vol1, pred_vol2, pred_vol3]
+#     pred_vol1 = np.array([[1, 1, 0], [0, 1, 1], [0, 0, 1]]) # acc = 7/9
+#     pred_vol2 = np.array([[1, 1, 0], [0, 0, 1], [1, 1, 0]]) #
+#     pred_vol3 = np.array([[1, 0, 0], [1, 0, 1], [0, 1, 0]]) #
+#     pred_tot = [pred_vol1, pred_vol2, pred_vol3]
 
-    fixed_names = ['fixed_array1', 'fixed_array2', 'fixed_array3']
-    moving_names = ['moving_array1', 'moving_array2', 'moving_array3']
+#     fixed_names = ['fixed_array1', 'fixed_array2', 'fixed_array3']
+#     moving_names = ['moving_array1', 'moving_array2', 'moving_array3']
 
-    a = AnalyzeVolume(GT_tot, pred_tot, fixed_names, moving_names)
-    all_metrics = a.evaluation_metrics()
-    metrics = all_metrics[0]
-    pdb.set_trace()
-    # print('fixed image:', metrics['fixed_image'], '\n moving image:', metrics['moving_image'], '\n DSC:',  metrics['Dice_score'])
-    print("fixed image:", metrics['fixed_image'], "\n moving image:", metrics['moving_image'],  "%.2f" % (metrics['Dice_score']))
+#     a = AnalyzeVolume(GT_tot, pred_tot, fixed_names, moving_names)
+#     all_metrics = a.evaluation_metrics()
+#     metrics = all_metrics[0]
+#     pdb.set_trace()
+#     # print('fixed image:', metrics['fixed_image'], '\n moving image:', metrics['moving_image'], '\n DSC:',  metrics['Dice_score'])
+#     print("fixed image:", metrics['fixed_image'], "\n moving image:", metrics['moving_image'],  "%.2f" % (metrics['Dice_score']))
 
